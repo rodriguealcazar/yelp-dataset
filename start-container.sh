0593f42c0b2b720e603044d45510c9daf17a93d7
Rodrigue <rodriguealcazar@gmail.com>
2018-10-11 14:33:13 +0200

    Remove container in script rather than automatically.

--- start-container.sh
+++ start-container.sh
@@ -4,5 +4,5 @@ set -e
 
 cd docker
 docker build . -t zeppelin-0.8.0:latest
-docker run -d --rm -p 8080:8080 -p 4040:4040 -p 4041:4041 --mount 'type=volume,src=yelp-spark,dst=/tmp/yelp-spark' zeppelin-0.8.0:latest
+docker run -d -p 8080:8080 -p 4040:4040 -p 4041:4041 --mount 'type=volume,src=yelp-spark,dst=/tmp/yelp-spark' zeppelin-0.8.0:latest
 cd ..
--- stop-container.sh
+++ stop-container.sh
@@ -7,5 +7,8 @@ DOCKER_CONTAINER_ID=`docker ps | grep zeppelin-0.8.0 | cut -d' ' -f1`
 echo "Stopping container"
 docker container stop ${DOCKER_CONTAINER_ID}
 
+echo "Deleting container"
+docker container rm ${DOCKER_CONTAINER_ID}
+
 echo "Deleting volume"
 docker volume rm yelp-spark
